f9504346b4de900bcb524c94fbdf724fe8da4e0e
rahul-velocis <rahul@velociscore.com>
2025-06-18 18:48:39 +0545

    Updated the fruit details page UI

--- lib/views/fruits_detail_page.dart
+++ lib/views/fruits_detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:new_app/models/fruit_model.dart';
 
 class FruitsDetailPage extends StatelessWidget {
@@ -9,16 +10,70 @@ class FruitsDetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Fruits Detail Page')),
+      backgroundColor: Colors.grey[200],
+
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Image.asset(fruit.imageUrl, height: 100, width: 100),
-            Text(
-              fruit.name,
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+            Image.asset(
+              fit: BoxFit.cover,
+
+              fruit.imageUrl,
+              height: 400,
+              width: MediaQuery.of(context).size.width,
             ),
-            Text(fruit.price, style: TextStyle(fontSize: 18)),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                spacing: 5,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Rs. ${fruit.price}",
+                        style: TextStyle(
+                          fontSize: 27,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.green,
+                        ),
+                      ),
+                      Icon(Icons.favorite_outline, size: 26, color: Colors.red),
+                    ],
+                  ),
+
+                  Text(
+                    fruit.name,
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+                  ),
+
+                  Row(
+                    children: [
+                      RatingBar.builder(
+                        itemSize: 30,
+                        initialRating: 2,
+                        minRating: 1,
+                        direction: Axis.horizontal,
+                        allowHalfRating: true,
+                        itemCount: 5,
+                        itemPadding: EdgeInsets.symmetric(horizontal: 0),
+                        itemBuilder: (context, _) =>
+                            Icon(Icons.star, color: Colors.amber),
+                        onRatingUpdate: (rating) {},
+                      ),
+
+                      Text(
+                        '(89 Reviews)',
+                        style: TextStyle(color: Colors.grey, fontSize: 20),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
