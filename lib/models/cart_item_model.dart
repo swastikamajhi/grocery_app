8cb79170228d06500c075ff891224f67f10f166b
RajanMajhi <rajanmajhi117@gmail.com>
2025-06-20 15:10:57 +0545

    added models

---
+++ lib/models/cart_item_model.dart
@@ -0,0 +1,9 @@
+class CartItemModel {
+  final String itemName;
+  final String itemprice;
+  final String itemQuantity;
+  final String itemImage;
+
+  CartItemModel({required this.itemName, required this.itemprice, required this.itemQuantity, required this.itemImage});
+
+}
--- lib/views/cart_page.dart
+++ lib/views/cart_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class CartPage extends StatelessWidget {
+  
   const CartPage({super.key});
 
   @override
