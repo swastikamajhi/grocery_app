ecdb73cb8c7c574b96b38ba7ff97077d6564d050
RajanMajhi <rajanmajhi117@gmail.com>
2025-07-01 17:26:56 +0545

    loginpage

---
+++ lib/auth/services/local_storage_service.dart
@@ -0,0 +1,14 @@
+import 'package:new_app/model/user_model.dart';
+
+abstract class LocalStorageService {
+  final bool isLoggedIn = false;
+
+  Future<void> saveUserData(UserModel model);
+
+  Future<UserModel> getUserData(UserModel model);
+
+  Future<bool> userLoggedIn();
+  //if user is logged in, true, navigate to homepage
+  // if user if not logged in ,false, navigate to login page
+//
+}
